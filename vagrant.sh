d43948a5afc02c5156aaf2f3dcf7070bf02198b2
Jean pasqualini <jpasqualini75@gmail.com>
2017-06-12 12:07:58 +0200

    Update vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -7,5 +7,5 @@ function vagrant:docker:up()
         TARGETDIR=$(echo $PWD | sed 's/\//\\\//g')
         sed -i 's/PWD/'$TARGETDIR'/g' Vagrantfile
         export DOCKER_HOST=tcp://192.168.33.10:2375
-        sudo vagrant up --provision
+        sudo vagrant up
 }
